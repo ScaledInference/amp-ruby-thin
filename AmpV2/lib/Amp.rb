4f0a7723f1f05f692820204377a1037103e0cc8a
Vidya Rangasayee <vidya@scaledinference.com>
2018-12-04 04:45:26 -0800

    Updating for additional checks

--- AmpV2/lib/Amp.rb
+++ AmpV2/lib/Amp.rb
@@ -8,9 +8,11 @@ class Amp
     	@apiPath = config['apiPath'] || '/api/core/v2/'
 	    @domain = config['domain']
 	    @options = config
-	    @timeout = config['timeout']
+	    @timeout = config['timeout'] || 10 * 1000
+        @sessionLifetime = config['sessionLifetime'] || 30 * 60 * 1000
 	    @version = '1.0.0'
 	    @project_key = config['key'] 
+        @dont_use_token = config['dont_use_token']
     end
 
     class << self
@@ -20,6 +22,9 @@ class Amp
 
     def Session(options = {})
     	options['amp'] = self
+        if @dont_use_token 
+            options['ampToken'] = "CUSTOM"
+        end
     	return Session.new(@domain, @apiPath, @project_key, options)
     end
 end
